51aabc765835252aa01d721cd272d783b5f94a13
Rohan Rambhiya <rohan@micronxt.com>
2025-06-07 17:22:20 +0530

    PO Preview Modal with Remarks Selling Purchase Editable

---
+++ erpnext_enhancements/custom_fields.py
@@ -0,0 +1,49 @@
+# File: apps/erpnext_enhancements/erpnext_enhancements/fixtures.py
+
+custom_fields = {
+    "Purchase Order": [
+        {
+            "fieldname": "remarks",
+            "label": "Remarks",
+            "fieldtype": "Text",
+            "insert_after": "terms",
+            "description": "Additional remarks for purchase order (e.g., Cash on Delivery, Special Instructions)",
+            "allow_in_quick_entry": 0,
+            "bold": 0,
+            "collapsible": 0,
+            "columns": 0,
+            "default": "",
+            "depends_on": "",
+            "description": "Enter any special remarks or instructions for this purchase order",
+            "fetch_from": "",
+            "fetch_if_empty": 0,
+            "hidden": 0,
+            "hide_border": 0,
+            "hide_days": 0,
+            "hide_seconds": 0,
+            "ignore_user_permissions": 0,
+            "ignore_xss_filter": 0,
+            "in_global_search": 0,
+            "in_list_view": 0,
+            "in_preview": 0,
+            "in_standard_filter": 0,
+            "length": 0,
+            "mandatory_depends_on": "",
+            "no_copy": 0,
+            "non_negative": 0,
+            "permlevel": 0,
+            "precision": "",
+            "print_hide": 0,
+            "print_hide_if_no_value": 0,
+            "print_width": "",
+            "read_only": 0,
+            "read_only_depends_on": "",
+            "report_hide": 0,
+            "reqd": 0,
+            "search_index": 0,
+            "translatable": 1,
+            "unique": 0,
+            "width": ""
+        }
+    ]
+}
