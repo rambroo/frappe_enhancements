0bed90877f58657ce36c6709e9aec0c1736e98dd
Rohan Rambhiya <rohan@micronxt.com>
2025-09-25 10:34:29 +0530

    Credit UI

--- erpnext_enhancements/custom_functions/credit_limit.py
+++ erpnext_enhancements/custom_functions/credit_limit.py
@@ -100,64 +100,48 @@ def get_credit_balance_2(customer, company):
 
 
 
+@frappe.whitelist()
+def get_credit_balance_3(customer, company):
+    outstanding_amt = get_customer_outstanding(
+        customer, company, ignore_outstanding_sales_order=False
+    )
+    credit_limit = get_credit_limit(customer, company) or 0
+    balance = credit_limit - outstanding_amt
+
+    invoices = frappe.db.sql("""
+        SELECT name, outstanding_amount
+        FROM `tabSales Invoice`
+        WHERE customer=%s AND company=%s AND docstatus=1 AND outstanding_amount > 0
+    """, (customer, company), as_dict=True)
+
+    sales_orders = frappe.db.sql("""
+        SELECT name, (rounded_total - advance_paid) AS pending_amount
+        FROM `tabSales Order`
+        WHERE customer=%s AND company=%s AND docstatus=1
+          AND status NOT IN ('Closed', 'Cancelled')
+    """, (customer, company), as_dict=True)
+
+    payments = frappe.db.sql("""
+        SELECT name, paid_amount, unallocated_amount
+        FROM `tabPayment Entry`
+        WHERE party_type='Customer' AND party=%s AND company=%s AND docstatus=1
+    """, (customer, company), as_dict=True)
+    
+    frappe.log_error(frappe.as_json({
+        "credit_limit": credit_limit,
+        "outstanding": outstanding_amt,
+        "balance": balance,
+        "invoices": invoices,
+        "sales_orders": sales_orders,
+        "payments": payments,
+    }), "DEBUG Credit Balance")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import frappe
-# from erpnext.selling.doctype.customer.customer import get_credit_limit
-
-# @frappe.whitelist()
-# def get_credit_balance(customer, company):
-#     """Return available credit balance for a customer 
-#        (Credit Limit - Net Unpaid - Pending SOs)"""
-
-#     # Net unpaid = debits - credits from GL (already invoiced & posted)
-#     outstanding_amt = frappe.db.sql(
-#         """
-#         SELECT SUM(debit_in_account_currency) - SUM(credit_in_account_currency)
-#         FROM `tabGL Entry`
-#         WHERE party_type = 'Customer'
-#           AND party = %s
-#           AND company = %s
-#           AND is_cancelled = 0
-#         """,
-#         (customer, company),
-#     )[0][0] or 0
-
-#     # Pending Sales Orders = approved/final submission SOs, not yet fully invoiced
-#     pending_so = frappe.db.sql(
-#         """
-#         SELECT SUM(base_grand_total * (1 - (per_billed / 100)))
-#         FROM `tabSales Order`
-#         WHERE customer = %s
-#           AND company = %s
-#           AND docstatus = 1
-#           AND workflow_state IN ("Approved", "Final Submission","Submitted")
-#         """,
-#         (customer, company),
-#     )[0][0] or 0
-
-#     # Credit limit defined for this customer
-#     credit_limit = get_credit_limit(customer, company) or 0
-
-#     # Remaining balance (credit - invoices - pending SOs)
-#     bal = credit_limit - (outstanding_amt + pending_so)
-
-#     return {
-#         "credit_limit": credit_limit,
-#         "outstanding_amt": outstanding_amt,
-#         "pending_so": pending_so,
-#         "balance": bal,
-#     }
+    return {
+        "credit_limit": credit_limit,
+        "outstanding": outstanding_amt,
+        "balance": balance,
+        "invoices": invoices,
+        "sales_orders": sales_orders,
+        "payments": payments,
+    }
