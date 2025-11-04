5a574516b59acd102e50331534371d40e2681d5d
Rohan Rambhiya <rohan@micronxt.com>
2025-11-04 17:04:49 +0530

    Sales Partner Commission Doctype added

---
+++ erpnext_enhancements/erpnext_enhancements/doctype/sales_partner_commission/test_sales_partner_commission.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Rohan and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSalesPartnerCommission(FrappeTestCase):
+	pass
